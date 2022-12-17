5fbe741367c4e7aaf2658ef159778515a0f784b1
yunlanli <chnsidney@gmail.com>
2022-12-16 20:48:38 -0500

    add dep optparse-applicate for args parse
    - added option --type/-t to sepcify mode of varys: parallel or
    sequential, defaults to parMap (i.e. parallel)

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 import Controller (parSebf, sebf)
+import Control.Monad (join)
 import Formatting (fprintLn)
 import Formatting.Clock (timeSpecs)
 import Generator
@@ -6,10 +7,51 @@ import Generator
     RandomSwitchSpec (..),
     generateProblem,
   )
+import Options.Applicative
 import System.Clock
+import System.Exit (die)
+
 
 main :: IO ()
-main = do
+main = join . customExecParser (prefs showHelpOnError) $
+  info (helper <*> parser)
+  (  fullDesc
+  <> header "ParVarys: Parallel Varys Coflow Scheduling Using SEBF "
+  <> progDesc ("Generates an offline coflow scheduling problem, and uses the " ++
+               "Varys Shortest Effective Bottlneck First heuristic to order "  ++
+               "the coflows.")
+  )
+  where
+    parser :: Parser (IO ())
+    parser =
+      work
+        <$> strOption
+            (  long "type"
+            <> short 't'
+            <> metavar "STRING"
+            <> help "varys mode: seq, parMap"
+            <> value "parMap"
+            <> showDefault
+            )
+        <*> option auto
+            (  long "number_param"
+            <> short 'n'
+            <> metavar "NUMBER"
+            <> help "number parameter"
+            <> value 1
+            <> showDefault
+            )
+
+work :: String -> Int -> IO ()
+work mode _ = do
+  sebfImpl <-
+    case mode of
+      "seq"    -> return sebf
+      "parMap" -> return parSebf
+      _        -> die $
+                    "Unrecognized varys mode: " ++
+                    "expect one of seq, parMap, got " ++ show mode
+
   let numCoflows = 4000
       numIngress = 1000
       numEgress = 1000
@@ -47,7 +89,7 @@ main = do
 
   problem <- generateProblem flowSpec ingressSwitchSpec egressSwitchSpec numIngress numEgress
   start <- getTime Monotonic
-  print $ parSebf problem
+  print $ sebfImpl problem
   end <- getTime Monotonic
   putStrLn "Calculation Time"
   fprintLn timeSpecs start end
