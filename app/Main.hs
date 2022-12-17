4ab1e8c069bd506547ff69c6287895043ed5e844
Etesam913 <etesamansari13@gmail.com>
2022-12-16 19:40:46 -0500

    ⏰ Added clock package to measure time of parSebf
    ➕ Added formatting package to print the timeSpecs Format type

--- app/Main.hs
+++ app/Main.hs
@@ -1,43 +1,53 @@
+import Controller (parSebf, sebf)
+import Formatting (fprintLn)
+import Formatting.Clock (timeSpecs)
 import Generator
-  ( RandomFlowSpec(..),
-    RandomSwitchSpec(..),
-    generateProblem)
-import Controller (parSebf)
+  ( RandomFlowSpec (..),
+    RandomSwitchSpec (..),
+    generateProblem,
+  )
+import System.Clock
 
 main :: IO ()
 main = do
-  let
-    numCoflows = 4000
-    numIngress = 1000
-    numEgress = 1000
-    flowSize = 2
-    minFlowsInSwitch = 0
-    maxflowsInSwitch = 5000
-    iBandwidth = 2
-    eBandwidth = 2
+  let numCoflows = 4000
+      numIngress = 1000
+      numEgress = 1000
+      flowSize = 2
+      minFlowsInSwitch = 0
+      maxflowsInSwitch = 5000
+      iBandwidth = 2
+      eBandwidth = 2
 
-    flowSpec = RandomFlowSpec {
-        minSwitchId = numIngress + 1,
-        maxSwitchId = numIngress + numEgress,
-        minCoflowId = 1,
-        maxCoflowId = numCoflows,
-        minFlowSize = flowSize,
-        maxFlowSize = flowSize
-    }
+      flowSpec =
+        RandomFlowSpec
+          { minSwitchId = numIngress + 1,
+            maxSwitchId = numIngress + numEgress,
+            minCoflowId = 1,
+            maxCoflowId = numCoflows,
+            minFlowSize = flowSize,
+            maxFlowSize = flowSize
+          }
 
-    ingressSwitchSpec = RandomSwitchSpec {
-        minFlows = minFlowsInSwitch,
-        maxFlows = maxflowsInSwitch,
-        ingressBandwidth = iBandwidth,
-        egressBandwidth = eBandwidth
-    }
+      ingressSwitchSpec =
+        RandomSwitchSpec
+          { minFlows = minFlowsInSwitch,
+            maxFlows = maxflowsInSwitch,
+            ingressBandwidth = iBandwidth,
+            egressBandwidth = eBandwidth
+          }
 
-    egressSwitchSpec = RandomSwitchSpec {
-        minFlows = 0,
-        maxFlows = 0,
-        ingressBandwidth = iBandwidth,
-        egressBandwidth = eBandwidth
-    }
+      egressSwitchSpec =
+        RandomSwitchSpec
+          { minFlows = 0,
+            maxFlows = 0,
+            ingressBandwidth = iBandwidth,
+            egressBandwidth = eBandwidth
+          }
 
   problem <- generateProblem flowSpec ingressSwitchSpec egressSwitchSpec numIngress numEgress
+  start <- getTime Monotonic
   print $ parSebf problem
+  end <- getTime Monotonic
+  putStrLn "Calculation Time"
+  fprintLn timeSpecs start end
